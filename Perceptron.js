78fa13534226614df0a2e2595d60682e7969c7db
Noor Aldeen <anooraldeen9@gmail.com>
2023-04-27 20:10:46 +0200

    called perceptron

--- Perceptron.js
+++ Perceptron.js
@@ -1,4 +1,4 @@
-const Perceptron = (learningRate, maxNumIterations, trainingData) => {
+export const Perceptron = (learningRate, maxNumIterations, trainingData) => {
   let w1 = Math.random() - 0.5;
   let w2 = Math.random() - 0.5;
   let threshold = 0.2;
@@ -22,5 +22,3 @@ function signFunction(x) {
     return -1;
   }
 }
-
-Perceptron(0.1,100);
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import {Perceptron} from './Perceptron.js'
 const workSpace = document.getElementById('workSpace')
 const classNum = document.getElementById('classNum')
 const classA = document.getElementById('classA')
@@ -8,13 +9,17 @@ const classAContainer = document.getElementById('classAContainer')
 const classBContainer = document.getElementById('classBContainer')
 const classCContainer = document.getElementById('classCContainer')
 const classDContainer = document.getElementById('classDContainer')
-
-let classType = 'A'
+const solveBtn = document.getElementById('solveBtn')
+const maxIteration = document.getElementById('maxIteration')
+const learningRate = document.getElementById('learningRate')
+const trainingData = [];
+let classType = 0
 let isStarted = false
 // Listeners
 workSpace.addEventListener('click', (e)=> {
   getClickPosition(e);
 })
+solveBtn.addEventListener('click',findWight)
 classNum.addEventListener('change',changeClassNum)
 classA.addEventListener('change',changeClassType)
 classB.addEventListener('change',changeClassType)
@@ -40,27 +45,29 @@ function changeClassNum(){
 function changeClassType(){
   if(classA.checked)
   {
-    classType = classA.value
+    classType = 0
   }
   else if(classB.checked)
   {
-    classType = classB.value
+    classType = 1
   }
   else if(classC.checked)
   {
-    classType = classC.value
+    classType = 2
   }
   else if(classD.checked)
   {
-    classType = classD.value
+    classType = 3
   }
 }
 
 function getClickPosition(e) {
     let p = {
-      x: e.clientX,
-      y: e.clientY
+      x1: e.clientX,
+      x2: e.clientY,
+      yd: classType
     }
+    trainingData.push(p);
     drawAt(p,"black",10);
     if(!isStarted){
       isStarted=true
@@ -72,58 +79,48 @@ function getClickPosition(e) {
 function drawAt(point,color,dotSize) {
   console.log(point);
   let div = document.createElement('div');
-  if(classType === 'A')
+  div.style.position = "absolute"
+  div.style.left = (point.x1 - dotSize / 2) + "px"
+  div.style.top = (point.x2 - dotSize / 2) + "px"
+
+  if(classType === 0)
   {
     div.style.backgroundColor = 'black'
     div.style.width = 10 + "px";
     div.style.height = 10 + "px"
-    div.style.position = "absolute"
-    div.style.left = (point.x - dotSize / 2) + "px"
-    div.style.top = (point.y - dotSize / 2) + "px"
     div.style.borderRadius = "50%"
     workSpace.appendChild(div);
   }
-  else if(classType === 'B')
+  else if(classType === 1)
   {
     div.style.backgroundColor = 'red'
     div.style.width = dotSize + "px";
     div.style.height = dotSize + "px"
-    div.style.position = "absolute"
-    div.style.left = (point.x - dotSize / 2) + "px"
-    div.style.top = (point.y - dotSize / 2) + "px"
     workSpace.appendChild(div);
   }
-  else if(classType === 'C')
+  else if(classType === 2)
   {
     div.style.width = 0 + "px";
     div.style.height = 0 + "px"
-    div.style.position = "absolute"
-    div.style.left = (point.x - dotSize / 2) + "px"
-    div.style.top = (point.y - dotSize / 2) + "px"
     div.style.borderLeft = "10px solid transparent"
     div.style.borderRight = "10px solid transparent"
     div.style.borderBottom = "10px solid green"
     workSpace.appendChild(div);
   }
-  else if(classType === 'D')
+  else if(classType === 3)
   {
     div.style.width = 12 + "px";
     div.style.height = 12 + "px"
-    div.style.position = "absolute"
     div.style.backgroundColor = 'blue'
-    div.style.left = (point.x - dotSize / 2) + "px"
-    div.style.top = (point.y - dotSize / 2) + "px"
     div.style.clipPath = "polygon( 50% 0, 100% 38%, 81% 100%, 19% 100%, 0 38%)"
     workSpace.appendChild(div);
   }
-  //***************** */
-  
-// ******
-  
 
-  //******* */
-
-  
 }
-  
+
+
+function findWight(){
+  let result = Perceptron(learningRate.value, maxIteration.value, trainingData)
+  console.log(result)
+}
   
