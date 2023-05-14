0a2e186970bdd09d937d0006c97ca083c5d14910
Noor Aldeen <anooraldeen9@gmail.com>
2023-05-14 16:07:00 +0300

    start working on confusion matrix

--- Perceptron.js
+++ Perceptron.js
@@ -20,7 +20,6 @@ export const Perceptron = (learningRate, maxNumIterations, trainingData, typeofC
     w2 += learningRate * error * x2;
     threshold += learningRate * -1 * error; // update bias term
   }
-  drawGraph(points,epochNum);
   console.log("points:  ",points)
   return { w1, w2, threshold, MSE}; // return bias as well
 };
@@ -34,40 +33,40 @@ function signFunction(x) {
   // return (1 / (1 +  Math.pow(Math.E,-x)));
 }
 
-const drawGraph=(data, epochNum)=>{
-  const canvas = document.getElementById("myCanvas");
-  const ctx = canvas.getContext("2d");
+// const drawGraph=(data, epochNum)=>{
+//   const canvas = document.getElementById("myCanvas");
+//   const ctx = canvas.getContext("2d");
   
-// Set up the graph axes
-const xMin = 0;
-const xMax = epochNum;
-const yMin = 0;
-const yMax = 0.01;
+// // Set up the graph axes
+// const xMin = 0;
+// const xMax = epochNum;
+// const yMin = 0;
+// const yMax = 0.01;
 
-// Define the scale factor for x and y values
-const xScale = canvas.width / (xMax - xMin);
-const yScale = canvas.height / (yMax - yMin);
+// // Define the scale factor for x and y values
+// const xScale = canvas.width / (xMax - xMin);
+// const yScale = canvas.height / (yMax - yMin);
 
-// Draw the x-axis
-ctx.beginPath();
-ctx.moveTo(0, canvas.height - yMin * yScale);
-ctx.lineTo(canvas.width, canvas.height - yMin * yScale);
-ctx.stroke();
+// // Draw the x-axis
+// ctx.beginPath();
+// ctx.moveTo(0, canvas.height - yMin * yScale);
+// ctx.lineTo(canvas.width, canvas.height - yMin * yScale);
+// ctx.stroke();
 
-// Draw the y-axis
-ctx.beginPath();
-ctx.moveTo(xMin * xScale, 0);
-ctx.lineTo(xMin * xScale, canvas.height);
-ctx.stroke();
+// // Draw the y-axis
+// ctx.beginPath();
+// ctx.moveTo(xMin * xScale, 0);
+// ctx.lineTo(xMin * xScale, canvas.height);
+// ctx.stroke();
 
-// Plot the data points
-ctx.beginPath();
-ctx.moveTo(data[0].x * xScale, canvas.height - data[0].y * yScale);
+// // Plot the data points
+// ctx.beginPath();
+// ctx.moveTo(data[0].x * xScale, canvas.height - data[0].y * yScale);
 
-for (let i = 1; i < data.length; i++) {
-  ctx.lineTo(data[i].x * xScale, canvas.height - data[i].y * yScale);
-}
+// for (let i = 1; i < data.length; i++) {
+//   ctx.lineTo(data[i].x * xScale, canvas.height - data[i].y * yScale);
+// }
 
-ctx.stroke();
+// ctx.stroke();
   
-}
+// }
